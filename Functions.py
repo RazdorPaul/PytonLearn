33285694a6b9593015e35f7e4dee85481465ff9a
RazdorPaul <99951001+RazdorPaul@users.noreply.github.com>
2024-05-05 22:11:23 +0900

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,12 @@
+def print_params(param):
+    print(param)
+    print(param)
+    return param
+
+flag = "вход"
+print("Тест-программа вывода через функцию. Для выхода введите слово "'"выход"')
+while flag.lower() != "выход":
+    flag = input("Введите строку:")
+    print_params(flag)
+
+
