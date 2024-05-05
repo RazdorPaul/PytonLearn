c372dcd3abd820536223d94828bd42fc8cb39306
RazdorPaul <99951001+RazdorPaul@users.noreply.github.com>
2024-05-05 19:49:12 +0900

    Add files via upload

---
+++ Cycles.py
@@ -0,0 +1,11 @@
+car_list=["Toyota Allion",
+          "Honda Accord",
+          "Mazda Demio",
+          "Mitsubishi Eclipce",
+          "Nissan Navarra"
+          ]
+cars_count = 0
+for i in range(len(car_list)):
+    cars_count += 10
+    print("Я езжу на машине", car_list[i])
+    print(cars_count)
