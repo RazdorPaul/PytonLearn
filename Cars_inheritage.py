1d1b971c047e640467ecab379e1d858b25a68db0
RazdorPaul <99951001+RazdorPaul@users.noreply.github.com>
2024-06-20 23:38:39 +0900

    Add files via upload

---
+++ Cars_inheritage.py
@@ -0,0 +1,24 @@
+class Car:
+    def __init__(self):
+        self.price = 1000000
+
+    def getPower(self):
+        return 1000
+
+class Vehicle:
+    def __init__(self):
+        self.vehicle_type = None
+
+class Nissan(Car, Vehicle):
+    def __init__(self):
+        self.price = 500000
+        self.vehicle_type = "sedan"
+
+    def getPower(self):
+        return super().getPower()
+
+
+car = Nissan()
+print(f"Тип кузова: {car.vehicle_type}")
+print(f"Стоимость авто: {car.price}")
+print(f"Мощность двигателя: {car.getPower()}")
