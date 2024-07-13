56ce8c852ceeb456d3565d13e02e759d9262b258
RazdorPaul <99951001+RazdorPaul@users.noreply.github.com>
2024-07-13 14:01:51 +0900

    Add files via upload

---
+++ Mod1.2.py
@@ -0,0 +1,6 @@
+tasks = 12
+time_of_work = 1.5
+time_average = time_of_work / tasks
+course = "Python"
+print(f"Курс: {course}, всего задач:{tasks}, затрачено часов: "
+      f"{time_of_work}, среднее время выполнения {time_average} часа.")
